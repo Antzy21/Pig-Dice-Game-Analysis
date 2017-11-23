008e30754b5242e5655874469f5a156337fc5b10
Anthony <Anthony@dunford.uk.com>
2017-11-23 15:06:28 +0000

    Introduce Top & Bottom size variables
    Better Code for clarity of what the function is doing

--- Probability/Matrices.m
+++ Probability/Matrices.m
@@ -3,20 +3,21 @@ function [C,D,n,m,stalemate]=Matrices(i,j,Dice_prob,To_win,A,B,P,Q)
 % Let j be Q's current score
 % Let k be the turn score
 
-Matrix_Size = 2*To_win-i-j;
-C=zeros(Matrix_Size,Matrix_Size);
-D=zeros(Matrix_Size,1);
+Top_size=To_win-i;
+Bottom_size=To_win-j;
+C=zeros(Top_size+Bottom_size,Top_size+Bottom_size);
+D=zeros(Top_size+Bottom_size,1);
 stalemate=false;
 
 % Build top half of the matrix
-for n= 1:To_win-i
+for n= 1:Top_size
     k=n-1;
     for z = 1:n % Checks all values above to see if variable is used.
         if C(z,n)~=0 || n == 1 % If not an impossible GS: variable is used.
             if A(i+1,j+1,k+1) == 0 % If Banking:
                 if k==0 % If Banking on 0... *sigh*
                     if B(j+1,i+1,1) == 0 % If Oppnt Banking on 0... *sigh*
-                        C=0; D=0; n=To_win-i; m=n+To_win-j; stalemate=true;
+                        C=0; D=0; n=Top_size; m=Top_size+Bottom_size; stalemate=true;
                         return
                     end
                     D(n)=1;
@@ -25,8 +26,8 @@ for n= 1:To_win-i
                     D(n)=1-Q(j+1,i+k+1,1);
                 end
             elseif A(i+1,j+1,k+1) == 1 % If Rolling:
-                C(n,To_win-i+1)=Dice_prob;
-                for z = 2:(To_win-i)-n
+                C(n,Top_size+1)=Dice_prob;
+                for z = 2:Top_size-n
                     if z <= (1/Dice_prob)
                         C(n,n+z)= -Dice_prob;
                     end
@@ -40,8 +41,8 @@ for n= 1:To_win-i
 end
 
 % Build bottom half of the matrix
-for m= n+1:n+To_win-j
-    k=m-(To_win-i)-1;
+for m= Top_size+1:Top_size+Bottom_size
+    k=m-Top_size-1;
     for z = n+1:m % Checks all values above to see if variable is used.
         if C(z,m)~=0 || m == n+1 % If not an impossible GS: variable is used.
             if B(j+1,i+1,k+1) == 0 % If Banking
@@ -53,7 +54,7 @@ for m= n+1:n+To_win-j
                 end
             elseif B(j+1,i+1,k+1) == 1 % If Rolling
                 C(m,1)=Dice_prob;
-                for z = 2:(To_win-j)-(m-n)
+                for z = 2:Top_size+Bottom_size-m
                     if z <= 1/Dice_prob
                         C(m,m+z)=-Dice_prob;
                     end
