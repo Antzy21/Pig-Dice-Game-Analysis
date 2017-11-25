7cda2d7e2a1ff151baac9f85e75b9af7fec88740
Anthony <Anthony@dunford.uk.com>
2017-11-25 15:25:58 +0000

    Third Attempt at Optimising

--- Optimising/Optimising_Matrices.m
+++ Optimising/Optimising_Matrices.m
@@ -1,61 +1,64 @@
-function [C,D,P_size,Q_size,stalemate]=Optimising_Matrices(i,j,Dice_prob,To_win,S,P,Q)
+function [C,D,P_size,Q_size,stalemate]=Optimising_Matrices(i,j,Dice_prob,To_win,Opt_Strat,Strategy,P,Q)
 % Let i be P's current score
 % Let j be Q's current score
 % Let k be the turn score
 
 P_size=To_win-i;
 Q_size=To_win-j;
-C=zeros(2*P_size+Q_size,2*P_size+Q_size);
-D=zeros(2*P_size+Q_size,1);
+C=zeros(P_size+Q_size,P_size+Q_size);
+D=zeros(P_size+Q_size,1);
 stalemate=false;
 
-
-for n= 1:2*P_size
-    if n <= To_win-i % Build top half of the matrix (For P rolling)
-        k=n-1; % Set value of turn score (k) for use and clarity
-        C(n,n)=1; % Identity Values
-        C(n,2*P_size+1)=Dice_prob; % If it looses, go to Q
-        for z = 2:(To_win-i)-n
-            if z <= (1/Dice_prob)
-                C(n,n+z)= -Dice_prob;
+% Build top half of the matrix (for P rolling)
+for n= 1:P_size
+    k=n-1; % Make k value for ease of reading and use
+    C(n,n)=1;
+    
+    % When Rolling:
+    C(2*P_size,2*P_size+1)=Dice_prob; % If it fails go to Q
+    for z = 2:P_size-n
+        if z <= (1/Dice_prob)
+            if Opt_Strat(i+1,j+1,k+z+1) == 1
+                C(n,n+z)= -Dice_prob; % If the next GS is to roll
+            elseif Opt_Strat(i+1,j+1,k+z+1) == 0
+                C(n,P_size+n+z)= -Dice_prob; % If the next GS is to bank
             end
         end
-        D(n)= sum(C(n,:))-Dice_prob; % Set value in D matrix
-    elseif n > To_win-i % Build top half of the matrix (For P banking)
-        k=n-2*P_size/2-1; % Set value of turn score (k) for use and clarity
-        C(n,n)=1; % Identity Values
-        if k==0 % If Banking on 0... *sigh*
-            if S(j+1,i+1,1) == 0 % If Oppnt Banking on 0... *sigh*
-                C=0; D=0; n=2*P_size; m=2*P_size+Q_size; stalemate=true;
-                return
-            end
-            D(n)=1; % Set value in D matrix for banking on 0
-            C(n,2*P_size+1)=1; % Goes to Q
-        else % Proper Bank:
-            D(n)=1-Q(j+1,i+k+1,1); % Set value in D matrix for normal bank
+    end
+    D(n)= sum(C(n,:))-Dice_prob; % Add in value for D matrix
+    % When Banking:
+    if k==0 % If Banking on 0... *sigh*
+        if Strategy(j+1,i+1,1) == 0 % If Oppnt Banking on 0... *sigh*
+            C=0; D=0; stalemate=true;
+            return
         end
+        D(P_size+n)=1; % Value for D matrix
+        C(P_size+n,2*P_size+1)=1; % Go straight to Q
+    else % Proper Bank:
+        D(P_size+n)=1-Q(j+1,i+k+1,1); % Value for D matrix
     end
 end
 
 % Build bottom half of the matrix
 for m= 2*P_size+1:2*P_size+Q_size
-    k=m-2*P_size-1; % Set value of turn score (k) for use and clarity
-    for z = 2*P_size+1:m % Checks all values above to see if variable is used.
-        if C(z,m)~=0 || m == n+1 % If not an impossible GS: variable is used.
-            if S(j+1,i+1,k+1) == 0 % If Banking
+    k=m-2*P_size-1; % Make k value for ease of reading and use
+    for z = P_size+1:m % Checks all values above to see if variable is used.
+        if C(z,m)~=0 || m == P_size+1 % If not an impossible GS: variable is used.
+            if Strategy(j+1,i+1,k+1) == 0 % If Banking
                 if k==0 % If Banking on 0... *sigh*
                     D(m)=1;
                     C(m,1)=1;
                 else % Proper Bank:
                     D(m)=1-P(i+1,j+k+1,1);
                 end
-            elseif S(j+1,i+1,k+1) == 1 % If Rolling
-                for z = 2:2*P_size+Q_size-m
+            elseif Strategy(j+1,i+1,k+1) == 1 % If Rolling
+                C(m,1)=Dice_prob;
+                for z = 2:P_size+Q_size-m
                     if z <= 1/Dice_prob
                         C(m,m+z)=-Dice_prob;
                     end
                 end
-                D(m)= 1+sum(C(m,:))-Dice_prob*P(i+1,j+1,k+1);
+                D(m)= 1+sum(C(m,:))-Dice_prob;
             end
             break
         end
--- Optimising/Optimising_Probability_Matrices.m
+++ Optimising/Optimising_Probability_Matrices.m
@@ -1,28 +1,26 @@
-function [P,Q,Optimised_Strategy]=Optimising_Probability_Matrices(Strategy_S,To_win,Dice_prob)
+function [P,Q]=Optimising_Probability_Matrices(Strategy_A,Strategy_B,To_win,Dice_prob)
 for i=1:2
 
-[Matrix_S,Name_S]=Strategies_to_Matrices(Strategy_S,To_win);
-    
-[P,Q]=Probability_Matrices(Strategy_S,Strategy_S,To_win,Dice_prob);
-Optimised_Strategy = zeros(To_win,To_win,To_win);
+% Set up P & Q probabilities as a matrix
+P=zeros(To_win,To_win,To_win);
+Q=zeros(To_win,To_win,To_win);
 
+% Set up Strategies as Matrices (If they aren't strategies already)
+[Opt_Strat,Name_A]=Strategies_to_Matrices(Strategy_A,To_win);
+[Strategy,Name_B]=Strategies_to_Matrices(Strategy_B,To_win);
 
 global_stalemate = false;
 for i=To_win-1:-1:0
     for j=To_win-1:-1:0
-        [C,D,P_size,Q_size,stalemate]=Optimising_Matrices(i,j,Dice_prob,To_win,Matrix_S,P,Q);
+        [C,D,P_size,Q_size,stalemate]=Matrices(i,j,Dice_prob,To_win,Opt_Strat,Strategy,P,Q);
         if stalemate == false
             X=C\D;
             for l=1:P_size
-                % Choose the max between Banking and Rolling
                 Proll=X(l);
                 Pbank=X(P_size+l);
                 P(i+1,j+1,l)=max(Proll,Pbank);
-                if Proll > Pbank % If rolling best option, choose roll
-                    Optimised_Strategy(i+1,j+1,l)=1;
-                end
             end
-            for l=1:Q_size-P_size
+            for l=P_size+1:P_size+Q_size
                 Q(j+1,i+1,l)=X(P_size+l);
             end
         elseif stalemate == true
@@ -30,7 +28,7 @@ for i=To_win-1:-1:0
             for l=1:P_size
                 P(i+1,j+1,l)=0;
             end
-            for l=1:Q_size-P_size
+            for l=P_size+1:P_size+Q_size
                 Q(j+1,i+1,l)=1;
             end
         end
@@ -38,12 +36,11 @@ for i=To_win-1:-1:0
 end
 
 if global_stalemate == false
-    fprintf('Probability of Optimised winning when going first is %f \n',P(1,1,1));
-    fprintf('Probability of ''%s'' winning when going first is %f \n',Name_S,Q(1,1,1));
+    fprintf('Probability of ''%s'' winning when going first is %f \n',Name_A,P(1,1,1));
+    fprintf('Probability of ''%s'' winning when going first is %f \n',Name_B,Q(1,1,1));
     return
 end
 fprintf('Probability of ''%s'' winning when going first is %f \n',Name_A,P(1,1,1));
 Strategy_temp = Strategy_A; Strategy_A = Strategy_B; Strategy_B = Strategy_temp;
 end
 end
-
