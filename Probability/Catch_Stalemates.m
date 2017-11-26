30807baa3b7d881e56014f5542c59996b39c557e
Anthony <Anthony@dunford.uk.com>
2017-11-26 18:57:16 +0000

    Correct strategy GS

--- Probability/Catch_Stalemates.m
+++ Probability/Catch_Stalemates.m
@@ -2,7 +2,7 @@ function [Stalemate,i,j]=Catch_Stalemates(A,B,To_win)
 Stalemate = false;
 for i=0:To_win-1
     for j=0:To_win-1
-        if A(i+1,j+1,1) == 0 && B(i+1,j+1,1) == 0
+        if A(i+1,j+1,1) == 0 && B(j+1,i+1,1) == 0
             Stalemate = true;
             return
         end
