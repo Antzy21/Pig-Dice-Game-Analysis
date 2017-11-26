e7b3b9ce4630b2dbdb609e616b266893724b7841
Anthony <Anthony@dunford.uk.com>
2017-11-26 12:24:24 +0000

    Include point at which Stalemate occurs

--- Probability/Catch_Stalemates.m
+++ Probability/Catch_Stalemates.m
@@ -1,9 +1,10 @@
-function Stalemate=Catch_Stalemates(A,B,To_win)
+function [Stalemate,i,j]=Catch_Stalemates(A,B,To_win)
 Stalemate = false;
-for i=0:To_win
-    for j=0:To_win
-        if A(i+1,j+1,0) == 0 && B(i+1,j+1,0) == 0
+for i=0:To_win-1
+    for j=0:To_win-1
+        if A(i+1,j+1,1) == 0 && B(i+1,j+1,1) == 0
             Stalemate = true;
+            return
         end
     end
 end
