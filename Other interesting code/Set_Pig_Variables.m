f8b824a76681494ebbd81ad7587220c7e26e9505
Anthony <Anthony@dunford.uk.com>
2017-11-27 16:10:37 +0000

    Correct Pig variables

--- Other interesting code/Set_Pig_Variables.m
+++ Other interesting code/Set_Pig_Variables.m
@@ -1,6 +1,6 @@
 function [Strategy_A,Strategy_B,To_win,Dice_prob]=Set_Pig_Variables()
-To_win     = 3;
-Dice_prob  = 1/2;
-Strategy_A = @Bank3;
-Strategy_B = @Bank2;
+To_win     = 100;
+Dice_prob  = 1/6;
+Strategy_A = @Bank20;
+Strategy_B = @Bank21;
 end
