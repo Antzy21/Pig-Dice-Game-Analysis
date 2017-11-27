ff4eaf72e78f9fe29cf60e93981670e2843d8389
Anthony <Anthony@dunford.uk.com>
2017-11-27 15:33:59 +0000

    correct bug
    Variable name has a capital where it shouldn't have

--- Other interesting code/Set_Pig_Variables.m
+++ Other interesting code/Set_Pig_Variables.m
@@ -1,4 +1,4 @@
-function [Strategy_A,Strategy_B,To_win,Dice_Prob]=Set_Pig_Variables()
+function [Strategy_A,Strategy_B,To_win,Dice_prob]=Set_Pig_Variables()
 To_win     = 3;
 Dice_prob  = 1/2;
 Strategy_A = @Bank3;
