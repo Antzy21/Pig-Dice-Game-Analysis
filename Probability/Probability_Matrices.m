4a1597fb0be6491696f423bba90b8a931bf2da01
Anthony <Anthony@dunford.uk.com>
2017-12-04 16:26:39 +0000

    Change Matrix_A/B to Strategy_A/B

--- Probability/Probability_Matrices.m
+++ Probability/Probability_Matrices.m
@@ -4,24 +4,22 @@ function [P,Q]=Probability_Matrices(Strategy_A,Strategy_B,To_win,Dice_prob)
 P=zeros(To_win,To_win,To_win);
 Q=zeros(To_win,To_win,To_win);
 
-inputname(1);
-
 % Set up Strategies as Matrices (If they aren't strategies already)
 if isa(Strategy_A, 'double')
     Name_A=inputname(1);
 elseif isa(Strategy_A, 'function_handle')
     Name_A=func2str(Strategy_A);
-    [Matrix_A]=Strategies_to_Matrices(Strategy_A,To_win);
+    [Strategy_A]=Strategies_to_Matrices(Strategy_A,To_win);
 end
 if isa(Strategy_B, 'double')
-    Name_B=inputname(1);
+    Name_B=inputname(2);
 elseif isa(Strategy_B, 'function_handle')
     Name_B=func2str(Strategy_B);
-    [Matrix_B]=Strategies_to_Matrices(Strategy_B,To_win);
+    [Strategy_B]=Strategies_to_Matrices(Strategy_B,To_win);
 end
 
 % Check for Stalemates
-[Stalemate,I,J]=Catch_Stalemates(Matrix_A,Matrix_B,To_win);
+[Stalemate,I,J]=Catch_Stalemates(Strategy_A,Strategy_B,To_win);
 if Stalemate == 1
     fprintf('A Stalemate has occured when ''%s'' has %d points and when ''%s'' has %d points\n',Name_A,I,Name_B,J);
     return
@@ -29,7 +27,7 @@ end
 
 for i=To_win-1:-1:0
     for j=To_win-1:-1:0
-        [C,D,P_size,Q_size]=Matrices(i,j,Dice_prob,To_win,Matrix_A,Matrix_B,P,Q);
+        [C,D,P_size,Q_size]=Matrices(i,j,Dice_prob,To_win,Strategy_A,Strategy_B,P,Q);
         X=C\D;
         for k=0:P_size-1 % Sort out P
             P(i+1,j+1,k+1)=X(k+1);
