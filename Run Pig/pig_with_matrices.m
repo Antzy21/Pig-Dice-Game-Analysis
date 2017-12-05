abddb4918928f369b68e2058d0c677f75003d6f2
Anthony <Anthony@dunford.uk.com>
2017-12-05 15:06:35 +0000

    Pig with Matrices
    Run the game of pig but with matrices allowed as inputs

---
+++ Run Pig/pig_with_matrices.m
@@ -0,0 +1,63 @@
+function winner = pig_with_matrices(opp1,opp2)
+
+    [M]=Strategies_to_Matrices(opp1,100);
+    [M]=Strategies_to_Matrices(opp2,100);
+
+    score1 = 0;
+    score2 = 0;
+    cont_game = 1;
+    
+    turn = 0;
+    
+    while cont_game
+        if opp1(score1,score2,0) == 0 && opp2(score2,score1,0) == 0
+            winner = 0;
+            return
+        end
+        while opp1(score1,score2,turn) == 1
+            throw = randi(6);
+            if throw == 1
+                turn = 0;
+                break
+            else
+                turn = turn + throw;
+                if score1 + turn > 99
+                    break
+                end
+            end
+        end
+        score1 = score1 + turn;
+        turn = 0;
+        %score = [score1, score2]
+        
+        if score1 < 100
+            while opp2(score2,score1,turn) == 1
+                throw = randi(6);
+                if throw == 1
+                    turn = 0;
+                    break
+                else
+                    turn = turn + throw;
+                    if score2 + turn > 99
+                        break
+                    end
+                end
+            end
+            score2 = score2 + turn;
+            turn = 0;
+            %score = [score1, score2]
+        end
+        
+        if score1 > 99
+            %disp('Player 1 wins the game')
+            cont_game = 0;
+            winner = 1;
+            return
+        elseif score2 > 99
+            %disp('Player 2 wins the game')
+            cont_game = 0;
+            winner = 2;
+            return
+        end
+    end
+end     
