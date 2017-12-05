342df5c8bc08633dca7f92c3a96051874bb17a8e
Anthony <Anthony@dunford.uk.com>
2017-12-05 17:54:58 +0000

    Check for correct matrix sizes in P_to_M

--- Probability/Strategies_to_Matrices.m
+++ Probability/Strategies_to_Matrices.m
@@ -1,10 +1,27 @@
 function [M]=Strategies_to_Matrices(S,To_win)
-M=zeros(To_win,To_win,To_win);
-for i=0:To_win-1
-    for j=0:To_win-1
-        for k=0:To_win-1
-            M(i+1,j+1,k+1)=S(i,j,k);
+if isa(S, 'function_handle')
+    M=zeros(To_win,To_win,To_win);
+    for i=0:To_win-1
+        for j=0:To_win-1
+            for k=0:To_win-1
+                M(i+1,j+1,k+1)=S(i,j,k);
+            end
         end
     end
+else
+    if size(S) == [To_win,To_win,To_win]
+        M=S;
+    else
+        disp('The Matrix is not the correct size for this game')
+        return
+    end
 end
+
+
+
+
+
+
+
+
 end
