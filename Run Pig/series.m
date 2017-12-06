96bca45aa9c3f6f5a198a765fc180c56eb5e7a27
Anthony <Anthony@dunford.uk.com>
2017-12-06 17:46:05 +0000

    Update Series code

--- Run Pig/series.m
+++ Run Pig/series.m
@@ -1,15 +1,20 @@
 function game_score = series ( strategy1, strategy2, total )
-	game_score = [0,0,0];
+	
+    % Set Gamescore to 0
+    game_score = [0,0,0];
 	
 	for i=1:total
         w = pig_with_matrices(strategy1,strategy2);
 		
-		if w == 1
+		if     w == 1
 			game_score(1) = game_score(1) + 1;
 		elseif w == 2
 			game_score(2) = game_score(2) + 1;
         elseif w == 0
             game_score(3) = game_score(3) + 1;
         end
+        
+        disp(game_score)
+        
 	end
 end
