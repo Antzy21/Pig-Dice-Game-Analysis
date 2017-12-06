0537826b18c7c2a250beffb9a8672742e4a2e3e3
Anthony <Anthony@dunford.uk.com>
2017-12-06 13:59:05 +0000

    Compare to Barries Results

--- Barrie_results_comparison.m
+++ Barrie_results_comparison.m
@@ -22,3 +22,15 @@ if abs(P(1,1,1) - 0.574) < 0.001
 else
     fprintf('Not the same: Bank on %d, %d to win\n',4,10)
 end
+[~,P,~]=Optimising_Probability_Matrices(@Roll,@Bank20,100,1/6);
+if abs(P(1,1,1) - 0.587367) < 0.001
+    fprintf('%f = 0.587367 \n',P(1,1,1))
+else
+    fprintf('Not the same: Bank on %d, %d to win\n',20,100)
+end
+[~,P,~]=Optimising_Probability_Matrices(@Roll,@Bank21,100,1/6);
+if abs(P(1,1,1) - 0.59718) < 0.001
+    fprintf('%f = 0.59718 \n',P(1,1,1))
+else
+    fprintf('Not the same: Bank on %d, %d to win\n',21,100)
+end
