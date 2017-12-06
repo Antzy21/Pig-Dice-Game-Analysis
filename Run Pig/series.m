303b40c171b9e0b6c41689156ced23e9a5f210ee
Anthony <Anthony@dunford.uk.com>
2017-12-06 17:18:31 +0000

    include pig with matrices

--- Run Pig/series.m
+++ Run Pig/series.m
@@ -2,7 +2,7 @@ function game_score = series ( strategy1, strategy2, total )
 	game_score = [0,0,0];
 	
 	for i=1:total
-        w = pig(strategy1,strategy2);
+        w = pig_with_matrices(strategy1,strategy2);
 		
 		if w == 1
 			game_score(1) = game_score(1) + 1;
